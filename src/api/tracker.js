6968f0beceb2517b662fdcd714aade4b9879e1de
Prateek Chauhan <prateek454@gmail.com>
2021-02-15 22:48:17 +0530

    Tracking app

---
+++ src/api/tracker.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { AsyncStorage } from "react-native";
+
+const instance = axios.create({
+  baseURL: "https://cbc47bcc7b2a.ngrok.io",
+});
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
